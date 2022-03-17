bfed0b957be75e4ffbfa349606b661bb1319be0f
Antoine Toulme <antoine@lunar-ocean.com>
2022-03-16 22:24:28 -0700

    simplify the code to produce a block

--- sov/src/main/java/com/sovereign/Network.kt
+++ sov/src/main/java/com/sovereign/Network.kt
@@ -20,33 +20,23 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import io.vertx.core.Vertx
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.apache.tuweni.blockprocessor.BlockProcessor
 import org.apache.tuweni.bytes.Bytes
 import org.apache.tuweni.bytes.Bytes32
 import org.apache.tuweni.crypto.Hash
-import org.apache.tuweni.eth.AccountState
 import org.apache.tuweni.eth.Address
 import org.apache.tuweni.eth.Block
-import org.apache.tuweni.eth.BlockBody
-import org.apache.tuweni.eth.BlockHeader
 import org.apache.tuweni.eth.EthJsonModule
-import org.apache.tuweni.eth.LogsBloomFilter
 import org.apache.tuweni.eth.Transaction
-import org.apache.tuweni.eth.TransactionReceipt
 import org.apache.tuweni.eth.repository.BlockchainRepository
-import org.apache.tuweni.evm.EVMExecutionStatusCode
 import org.apache.tuweni.evm.EthereumVirtualMachine
 import org.apache.tuweni.evm.impl.EvmVmImpl
 import org.apache.tuweni.genesis.Genesis
 import org.apache.tuweni.plumtree.EphemeralPeerRepository
 import org.apache.tuweni.plumtree.Peer
 import org.apache.tuweni.plumtree.vertx.VertxGossipServer
-import org.apache.tuweni.rlp.RLP
-import org.apache.tuweni.trie.MerklePatriciaTrie
-import org.apache.tuweni.trie.MerkleTrie
 import org.apache.tuweni.units.bigints.UInt256
 import org.apache.tuweni.units.bigints.UInt64
-import org.apache.tuweni.units.ethereum.Gas
-import org.apache.tuweni.units.ethereum.Wei
 import java.time.Instant
 import java.util.Timer
 import java.util.TimerTask
@@ -176,132 +166,10 @@ data class BlockProducer(
       object : TimerTask() {
         override fun run() = runBlocking {
           val genesisBlock = repository.retrieveGenesisBlock()
-          var index = 0L
 
-          val bloomFilter = LogsBloomFilter()
-
-          val transactionsTrie = MerklePatriciaTrie.storingBytes()
-          val receiptsTrie = MerklePatriciaTrie.storingBytes()
-          val allReceipts = mutableListOf<TransactionReceipt>()
-
-          var counter = 0L
-          var allGasUsed = Gas.ZERO
-          for (tx in initialTransactions) {
-            val indexKey = RLP.encodeValue(UInt256.valueOf(counter).trimLeadingZeros())
-            transactionsTrie.put(indexKey, tx.toBytes())
-            if (null == tx.to) {
-              val contractAddress = Address.fromBytes(
-                Hash.keccak256(
-                  RLP.encodeList {
-                    it.writeValue(tx.sender!!)
-                    it.writeValue(tx.nonce)
-                  }
-                ).slice(12)
-              )
-              val state = AccountState(
-                UInt256.ONE,
-                Wei.valueOf(0),
-                org.apache.tuweni.eth.Hash.fromBytes(MerkleTrie.EMPTY_TRIE_ROOT_HASH),
-                org.apache.tuweni.eth.Hash.hash(tx.payload)
-              )
-              repository.storeAccount(contractAddress, state)
-              repository.storeCode(tx.payload)
-              val receipt = TransactionReceipt(
-                1,
-                0, // TODO
-                LogsBloomFilter(),
-                emptyList()
-              )
-              allReceipts.add(receipt)
-              receiptsTrie.put(indexKey, receipt.toBytes())
-              counter++
-            } else {
-              val code = repository.getAccountCode(tx.to!!)
-              val result = vm.execute(
-                tx.sender!!,
-                tx.to!!,
-                tx.value,
-                code!!,
-                tx.payload,
-                genesisBlock.header.gasLimit,
-                tx.gasPrice,
-                Address.ZERO,
-                index,
-                Instant.now().toEpochMilli(),
-                tx.gasLimit.toLong(),
-                genesisBlock.header.difficulty
-              )
-              if (result.statusCode != EVMExecutionStatusCode.SUCCESS) {
-                throw Exception("invalid transaction result")
-              }
-              for (balanceChange in result.changes.getBalanceChanges()) {
-                val state = repository.getAccount(balanceChange.key)?.let {
-                  AccountState(it.nonce, balanceChange.value, it.storageRoot, it.codeHash)
-                } ?: repository.newAccountState()
-                repository.storeAccount(balanceChange.key, state)
-              }
-
-              for (storageChange in result.changes.getAccountChanges()) {
-                for (oneStorageChange in storageChange.value) {
-                  repository.storeAccountValue(storageChange.key, oneStorageChange.key, oneStorageChange.value)
-                }
-              }
-
-              for (accountToDestroy in result.changes.accountsToDestroy()) {
-                repository.destroyAccount(accountToDestroy)
-              }
-              for (log in result.changes.getLogs()) {
-                bloomFilter.insertLog(log)
-              }
-
-              val txLogsBloomFilter = LogsBloomFilter()
-              for (log in result.changes.getLogs()) {
-                bloomFilter.insertLog(log)
-              }
-              val receipt = TransactionReceipt(
-                1,
-                result.gasManager.gasCost.toLong(),
-                txLogsBloomFilter,
-                result.changes.getLogs()
-              )
-              allReceipts.add(receipt)
-              receiptsTrie.put(indexKey, receipt.toBytes())
-              counter++
-
-              allGasUsed = allGasUsed.add(result.gasManager.gasCost)
-            }
-            repository.storeTransaction(tx)
-          }
-
-          // create a block from initial transactions, and execute them:
-          val block = Block(
-            BlockHeader(
-              genesisBlock.header.hash,
-              Genesis.emptyListHash,
-              Address.ZERO,
-              org.apache.tuweni.eth.Hash.fromBytes(repository.worldState!!.rootHash()),
-              org.apache.tuweni.eth.Hash.fromBytes(transactionsTrie.rootHash()),
-              org.apache.tuweni.eth.Hash.fromBytes(receiptsTrie.rootHash()),
-              bloomFilter.toBytes(),
-              genesisBlock.header.difficulty,
-              genesisBlock.header.number.add(1),
-              genesisBlock.header.gasLimit,
-              allGasUsed,
-              Instant.now(),
-              Bytes.EMPTY,
-              org.apache.tuweni.eth.Hash.fromBytes(Bytes32.random()),
-              UInt64.random()
-            ),
-            BlockBody(initialTransactions, listOf())
-          )
-
-          for (i in 0..(initialTransactions.size - 1)) {
-            val receipt = allReceipts[i]
-            val tx = initialTransactions[i]
-            repository.storeTransactionReceipt(receipt, 0, tx.hash, block.header.hash)
-            repository.storeTransaction(tx)
-          }
-          repository.storeBlock(block)
+          val processor = BlockProcessor()
+          val protoBlock = processor.execute(genesisBlock, initialTransactions, repository)
+          val block = protoBlock.toBlock(listOf(), Address.ZERO, UInt256.ONE, Instant.now(), Bytes.EMPTY, Genesis.emptyHash, UInt64.random())
           // send to all the nodes
           sendToFullNodes(block)
           // wait a second before propagating the state root
